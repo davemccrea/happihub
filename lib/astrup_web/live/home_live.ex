cdad5887ad67a8954bad1ab6e5cdbcaa1d4141cf
David McCrea <mail@dmccrea.me>
2025-05-26 20:04:39 +0300

    Add IDs for to help LiveView with diffing, tweak layout

--- lib/astrup_web/live/home_live.ex
+++ lib/astrup_web/live/home_live.ex
@@ -30,174 +30,176 @@ defmodule AstrupWeb.HomeLive do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash}>
-      <div class="flex flex-row gap-8 justify-center">
-        <article class="relative max-w-2xl select-none bg-base-200 py-12 px-12 shadow-xl border border-base-content/10">
-          <header class="text-center">
-            <h1 class="text-3xl font-serif font-medium mb-6">RADIOMETER ABL90 SERIES</h1>
-            <div class="space-y-1">
-              <div class="flex justify-between">
-                <span>ABL90 ABL TeVa I393-092R0178N0019</span>
-                <time>{Calendar.strftime(@sample_date, "%H:%M")}</time>
-                <time>{Calendar.strftime(@sample_date, "%d.%m.%Y")}</time>
-              </div>
-              <div class="flex justify-between">
-                <span>PATIENT REPORT</span>
-                <span>Syringe - S 65uL</span>
-                <span>Sample #</span>
-                <span>{@sample_number}</span>
-              </div>
-            </div>
-          </header>
-
-          <hr class="border-[1.5px] mb-1 mt-1" />
-
-          <section class="px-2">
-            <.heading label="Identifications" />
-            <dl class="ml-4">
-              <div class="grid grid-cols-[1fr_2fr] gap-4">
-                <dt>Patient ID</dt>
-                <dd>XXXXXX-XXXX</dd>
-              </div>
-              <div class="grid grid-cols-[1fr_2fr] gap-4">
-                <dt>Sample type</dt>
-                <dd>Arterial</dd>
-              </div>
-              <div class="grid grid-cols-[1fr_2fr] gap-4">
-                <dt class="italic">T</dt>
-                <dd>37,0 °C</dd>
-              </div>
-            </dl>
-          </section>
-
-          <hr class="mb-1 mt-2 border-[1.5px]" />
-
-          <div class="px-4">
-            <section class="mb-1">
-              <.heading label="Temperature-corrected values" />
-              <dl class="space-y-1 ml-8">
-                <.parameter parameter_id={0} {assigns}>
-                  <:label>pH(<i> T </i>)</:label>
-                </.parameter>
-                <.parameter parameter_id={1} {assigns}>
-                  <:label>
-                    <i>p</i>CO<sub>2</sub>(<i> T </i>)
-                  </:label>
-                </.parameter>
-                <.parameter parameter_id={2} {assigns}>
-                  <:label>
-                    <i>p</i>O<sub>2</sub>(<i> T </i>)
-                  </:label>
-                </.parameter>
-              </dl>
-            </section>
-
-            <section class="mb-1">
-              <.heading label="Acid-base status" />
-              <dl class="space-y-1 ml-8">
-                <.parameter parameter_id={3} {assigns}>
-                  <:label><i>c</i>HCO<sub>3</sub><sup>-</sup>(P)<i><sub>c</sub></i></:label>
-                </.parameter>
-                <.parameter parameter_id={4} {assigns}>
-                  <:label><i>c</i>Base(Ecf)<i><sub>c</sub></i></:label>
-                </.parameter>
-                <.parameter parameter_id={5} {assigns}>
-                  <:label>Anion Gap<i><sub>c</sub></i></:label>
-                </.parameter>
-              </dl>
-            </section>
-
-            <section class="mb-1">
-              <.heading label="Oximetry values" />
-              <dl class="space-y-1 ml-8">
-                <.parameter parameter_id={6} {assigns}>
-                  <:label><i>c</i>tHb</:label>
-                </.parameter>
-                <.parameter parameter_id={7} {assigns}>
-                  <:label><i>c</i>tO<sub>2</sub><i>c</i></:label>
-                </.parameter>
-                <.parameter parameter_id={8} {assigns}>
-                  <:label><i>s</i>O<sub>2</sub></:label>
-                </.parameter>
-                <.parameter parameter_id={9} {assigns}>
-                  <:label><i>F</i>COHb</:label>
-                </.parameter>
-                <.parameter parameter_id={10} {assigns}>
-                  <:label><i>F</i>MetHb</:label>
-                </.parameter>
-              </dl>
-            </section>
-
-            <section class="mb-1">
-              <.heading label="Electrolyte values" />
-              <dl class="space-y-1 ml-8">
-                <.parameter parameter_id={11} {assigns}>
-                  <:label><i>c</i>K<sup>+</sup></:label>
-                </.parameter>
-                <.parameter parameter_id={12} {assigns}>
-                  <:label><i>c</i>Na<sup>+</sup></:label>
-                </.parameter>
-                <.parameter parameter_id={13} {assigns}>
-                  <:label><i>c</i>Ca<sup>2+</sup></:label>
-                </.parameter>
-                <.parameter parameter_id={14} {assigns}>
-                  <:label><i>c</i>Ca<sup>2+</sup>(7.4)<i>c</i></:label>
-                </.parameter>
-                <.parameter parameter_id={15} {assigns}>
-                  <:label><i>c</i>Cl<sup>-</sup></:label>
-                </.parameter>
-              </dl>
-            </section>
-
-            <section class="mb-1">
-              <.heading label="Metabolite values" />
-              <dl class="space-y-1 ml-8">
-                <.parameter parameter_id={16} {assigns}>
-                  <:label><i>c</i>Glu</:label>
-                </.parameter>
-                <.parameter parameter_id={17} {assigns}>
-                  <:label><i>c</i>Lac</:label>
-                </.parameter>
-              </dl>
-            </section>
-          </div>
-
-          <hr class="border-dashed mb-1" />
-
-          <section class="mb-2">
-            <.heading label="Notes" />
-            <dl>
-              <div class="flex flex-row gap-24">
-                <dt class="italic">c</dt>
-                <dd>Calculated value(s)</dd>
-              </div>
-            </dl>
-          </section>
-
-          <hr class="mb-14 border-[1.5px]" />
-
-          <hr class="mb-2 border-[1.5px]" />
-
-          <footer>
-            <div class="flex justify-between">
-              <div>
-                <div>Solution pack lot: DX-20</div>
-                <div class="flex flex-row gap-12">
-                  <span>Printed</span>
-                  <time datetime={@printed_date}>{Calendar.strftime(@printed_date, "%H:%M")}</time>
-                  <time datetime={@printed_date}>
-                    {Calendar.strftime(@printed_date, "%d.%m.%Y")}
-                  </time>
+      <div class="flex flex-row gap-8 justify-between">
+        <div class="flex-grow flex justify-center">
+          <article class="relative max-w-2xl select-none bg-base-200 py-12 px-12 shadow-xl border border-base-content/10">
+            <header class="text-center">
+              <h1 class="text-3xl font-serif font-medium mb-6">RADIOMETER ABL90 SERIES</h1>
+              <div class="space-y-1">
+                <div class="flex justify-between">
+                  <span>ABL90 ABL TeVa I393-092R0178N0019</span>
+                  <time>{Calendar.strftime(@sample_date, "%H:%M")}</time>
+                  <time>{Calendar.strftime(@sample_date, "%d.%m.%Y")}</time>
+                </div>
+                <div class="flex justify-between">
+                  <span>PATIENT REPORT</span>
+                  <span>Syringe - S 65uL</span>
+                  <span>Sample #</span>
+                  <span>{@printout.id}</span>
                 </div>
               </div>
-              <div class="text-right">
-                <div>Sensor cassette run #: 2496-39</div>
-              </div>
+            </header>
+
+            <hr class="border-[1.5px] mb-1 mt-1" />
+
+            <section class="px-2">
+              <.heading label="Identifications" />
+              <dl class="ml-4">
+                <div class="grid grid-cols-[1fr_2fr] gap-4">
+                  <dt>Patient ID</dt>
+                  <dd>XXXXXX-XXXX</dd>
+                </div>
+                <div class="grid grid-cols-[1fr_2fr] gap-4">
+                  <dt>Sample type</dt>
+                  <dd>Arterial</dd>
+                </div>
+                <div class="grid grid-cols-[1fr_2fr] gap-4">
+                  <dt class="italic">T</dt>
+                  <dd>37,0 °C</dd>
+                </div>
+              </dl>
+            </section>
+
+            <hr class="mb-1 mt-2 border-[1.5px]" />
+
+            <div class="px-4">
+              <section class="mb-1">
+                <.heading label="Temperature-corrected values" />
+                <dl class="space-y-1 ml-8">
+                  <.parameter parameter_id={0} {assigns}>
+                    <:label>pH(<i> T </i>)</:label>
+                  </.parameter>
+                  <.parameter parameter_id={1} {assigns}>
+                    <:label>
+                      <i>p</i>CO<sub>2</sub>(<i> T </i>)
+                    </:label>
+                  </.parameter>
+                  <.parameter parameter_id={2} {assigns}>
+                    <:label>
+                      <i>p</i>O<sub>2</sub>(<i> T </i>)
+                    </:label>
+                  </.parameter>
+                </dl>
+              </section>
+
+              <section class="mb-1">
+                <.heading label="Acid-base status" />
+                <dl class="space-y-1 ml-8">
+                  <.parameter parameter_id={3} {assigns}>
+                    <:label><i>c</i>HCO<sub>3</sub><sup>-</sup>(P)<i><sub>c</sub></i></:label>
+                  </.parameter>
+                  <.parameter parameter_id={4} {assigns}>
+                    <:label><i>c</i>Base(Ecf)<i><sub>c</sub></i></:label>
+                  </.parameter>
+                  <.parameter parameter_id={5} {assigns}>
+                    <:label>Anion Gap<i><sub>c</sub></i></:label>
+                  </.parameter>
+                </dl>
+              </section>
+
+              <section class="mb-1">
+                <.heading label="Oximetry values" />
+                <dl class="space-y-1 ml-8">
+                  <.parameter parameter_id={6} {assigns}>
+                    <:label><i>c</i>tHb</:label>
+                  </.parameter>
+                  <.parameter parameter_id={7} {assigns}>
+                    <:label><i>c</i>tO<sub>2</sub><i>c</i></:label>
+                  </.parameter>
+                  <.parameter parameter_id={8} {assigns}>
+                    <:label><i>s</i>O<sub>2</sub></:label>
+                  </.parameter>
+                  <.parameter parameter_id={9} {assigns}>
+                    <:label><i>F</i>COHb</:label>
+                  </.parameter>
+                  <.parameter parameter_id={10} {assigns}>
+                    <:label><i>F</i>MetHb</:label>
+                  </.parameter>
+                </dl>
+              </section>
+
+              <section class="mb-1">
+                <.heading label="Electrolyte values" />
+                <dl class="space-y-1 ml-8">
+                  <.parameter parameter_id={11} {assigns}>
+                    <:label><i>c</i>K<sup>+</sup></:label>
+                  </.parameter>
+                  <.parameter parameter_id={12} {assigns}>
+                    <:label><i>c</i>Na<sup>+</sup></:label>
+                  </.parameter>
+                  <.parameter parameter_id={13} {assigns}>
+                    <:label><i>c</i>Ca<sup>2+</sup></:label>
+                  </.parameter>
+                  <.parameter parameter_id={14} {assigns}>
+                    <:label><i>c</i>Ca<sup>2+</sup>(7.4)<i>c</i></:label>
+                  </.parameter>
+                  <.parameter parameter_id={15} {assigns}>
+                    <:label><i>c</i>Cl<sup>-</sup></:label>
+                  </.parameter>
+                </dl>
+              </section>
+
+              <section class="mb-1">
+                <.heading label="Metabolite values" />
+                <dl class="space-y-1 ml-8">
+                  <.parameter parameter_id={16} {assigns}>
+                    <:label><i>c</i>Glu</:label>
+                  </.parameter>
+                  <.parameter parameter_id={17} {assigns}>
+                    <:label><i>c</i>Lac</:label>
+                  </.parameter>
+                </dl>
+              </section>
             </div>
-          </footer>
-        </article>
+
+            <hr class="border-dashed mb-1" />
+
+            <section class="mb-2">
+              <.heading label="Notes" />
+              <dl>
+                <div class="flex flex-row gap-24">
+                  <dt class="italic">c</dt>
+                  <dd>Calculated value(s)</dd>
+                </div>
+              </dl>
+            </section>
+
+            <hr class="mb-14 border-[1.5px]" />
+
+            <hr class="mb-2 border-[1.5px]" />
+
+            <footer>
+              <div class="flex justify-between">
+                <div>
+                  <div>Solution pack lot: DX-20</div>
+                  <div class="flex flex-row gap-12">
+                    <span>Printed</span>
+                    <time datetime={@printed_date}>{Calendar.strftime(@printed_date, "%H:%M")}</time>
+                    <time datetime={@printed_date}>
+                      {Calendar.strftime(@printed_date, "%d.%m.%Y")}
+                    </time>
+                  </div>
+                </div>
+                <div class="text-right">
+                  <div>Sensor cassette run #: 2496-39</div>
+                </div>
+              </div>
+            </footer>
+          </article>
+        </div>
 
         <div class="sticky top-4 self-start space-y-4 w-64">
-          <h2 class="text-xl font-semibold mb-3">ABG Quiz</h2>
+          <h2 class="text-xl font-semibold mb-3">Reference Values Quiz</h2>
           <p class="text-sm mb-4">
             For each parameter on the left, select whether the value is Low (L), Normal (N), or High (H) compared to its reference range.
             Once you've made all 18 selections, click "Check Answers".
@@ -219,8 +221,8 @@ defmodule AstrupWeb.HomeLive do
             </button>
           </div>
           <div class="mt-4 pt-4 border-t">
-            <p class="text-lg font-semibold">
-              Selections: {number_of_selections_made(@selections)} / 18
+            <p>
+              Selections: {number_of_selections_made(@selections)}/18
             </p>
             <%= if @test_state == :result do %>
               <.score selections={@selections} />
@@ -245,7 +247,7 @@ defmodule AstrupWeb.HomeLive do
     ~H"""
     <% {selection, correct_answer?} = @selections[@parameter_id] %>
 
-    <div class="grid grid-cols-[1fr_1fr_1fr_1fr] gap-4">
+    <div id={"param-#{@parameter_id}"} class="grid grid-cols-[1fr_1fr_1fr_1fr] gap-4">
       <dt>
         <div class="tooltip tooltip-right" data-tip={Astrup.get_parameter_label(@parameter_id)}>
           {render_slot(@label)}
@@ -258,6 +260,7 @@ defmodule AstrupWeb.HomeLive do
 
       <dd class="flex flex-row gap-1 items-center">
         <div
+          id={"tooltip-param-#{@parameter_id}"}
           class={
             if(show_hint?(@test_state, correct_answer?),
               do: "tooltip tooltip-right tooltip-open",
@@ -267,6 +270,7 @@ defmodule AstrupWeb.HomeLive do
           data-tip={Astrup.pretty_print_reference_range(@parameter_id)}
         >
           <button
+            id={"btn-param-#{@parameter_id}-low"}
             phx-click="select"
             phx-value-choice="low"
             phx-value-parameter_id={@parameter_id}
@@ -279,6 +283,7 @@ defmodule AstrupWeb.HomeLive do
           </button>
 
           <button
+            id={"btn-param-#{@parameter_id}-normal"}
             phx-click="select"
             phx-value-choice="normal"
             phx-value-parameter_id={@parameter_id}
@@ -291,6 +296,7 @@ defmodule AstrupWeb.HomeLive do
           </button>
 
           <button
+            id={"btn-param-#{@parameter_id}-high"}
             phx-click="select"
             phx-value-choice="high"
             phx-value-parameter_id={@parameter_id}
