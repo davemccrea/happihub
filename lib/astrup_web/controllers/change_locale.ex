e9901f85d3c7f967f6ffe0abe144daacc3dfa45c
David McCrea <mail@dmccrea.me>
2025-06-29 05:06:53 +0300

    Redirect to current page when changing locale

--- lib/astrup_web/controllers/change_locale.ex
+++ lib/astrup_web/controllers/change_locale.ex
@@ -5,8 +5,15 @@ defmodule AstrupWeb.ChangeLocale do
   def index(conn, %{"locale" => locale}) do
     Logger.info("Setting locale to #{locale}")
 
+    referrer = get_req_header(conn, "referer") |> List.first() || "/"
+    redirect_path = if referrer && String.contains?(referrer, conn.host) do
+      URI.parse(referrer).path || "/"
+    else
+      "/"
+    end
+
     conn
     |> put_session(:locale, locale)
-    |> redirect(to: "/")
+    |> redirect(to: redirect_path)
   end
 end
